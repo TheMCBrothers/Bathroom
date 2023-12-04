688e4512bd7d3c462722c09b8a36e7cb8213c170
TheMCLoveMan <themcloveman@gmail.com>
2023-12-04 22:10:51 +0100

    starting implementation of toilet block

--- src/main/java/net/themcbrothers/bathroom/infrastructure/BathroomBlocks.java
+++ src/main/java/net/themcbrothers/bathroom/infrastructure/BathroomBlocks.java
@@ -1,9 +1,11 @@
 package net.themcbrothers.bathroom.infrastructure;
 
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.registries.DeferredBlock;
+import net.themcbrothers.bathroom.block.ToiletBlock;
 
 import java.util.function.Function;
 
@@ -16,13 +18,15 @@ public final class BathroomBlocks {
 
     public static final DeferredBlock<Block> EXAMPLE_BLOCK = registerBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
 
+    public static final DeferredBlock<Block> TOILET = registerBlock("toilet", ToiletBlock::new, BlockBehaviour.Properties.of().mapColor(DyeColor.WHITE));
+
     private static DeferredBlock<Block> registerBlock(String name, BlockBehaviour.Properties props) {
         return registerBlock(name, Block::new, props);
     }
 
     private static <B extends Block> DeferredBlock<B> registerBlock(String name, Function<BlockBehaviour.Properties, ? extends B> func, BlockBehaviour.Properties props) {
         DeferredBlock<B> deferredBlock = BLOCKS.registerBlock(name, func, props);
-        ITEMS.registerSimpleBlockItem(name, deferredBlock);
+        ITEMS.registerSimpleBlockItem(deferredBlock);
         return deferredBlock;
     }
 }
